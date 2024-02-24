a184bca512f7859c9dfd81c60614ca3aff5ac1ce
Rehan <github@rehanzo.com>
2024-02-23 22:42:49 -0500

    web up

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from hackernews import HackerNews
 import nltk
 from nltk.stem import WordNetLemmatizer
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import asyncio
 
 flask = Flask(__name__)
 
 def hn_parse(limit=500):
+    # Create seperate event loop for hn
+    # else can conflict with flask
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+
     hn = HackerNews()
     top_stories = hn.top_stories(limit=limit)
+
     # Closing event loop
     loop.close()
+
     return top_stories
+
 def lemmatize(string):
     # tokenize
     words = nltk.word_tokenize(string)
@@ -32,22 +38,26 @@ def lemmatize(string):
     wnl = WordNetLemmatizer()
     lemmas = [wnl.lemmatize(word) for word in keywords]
 
-    # dedup
+    # deduplicate lemmas
     lemmas = list(dict.fromkeys(lemmas))
     return lemmas
     
 def relevance_reorder(stories, bio):
     bio_l = lemmatize(bio)
+
+    # array with (story, score) tuples
     story_score = []
+
     for story in stories:
-        # get common key words
         title_l = lemmatize(story.title)
+        # get common lemmas between title and bio
         common_title = list(set(title_l) & set(bio_l))
         # title is scored higher, since title words are generally more relevant to subject matter
         score = len(common_title) * 2
 
         # score with text, links posts don't have text
         if story.text is not None:
+            # do same scoring with text, but not *2
             text_l = lemmatize(story.text)
             common_text = list(set(text_l) & set(bio_l))
             score += len(common_text)
@@ -58,12 +68,14 @@ def relevance_reorder(stories, bio):
     return [e[0] for e in sorted(story_score, key=lambda x: x[1], reverse=True)]
 
 @flask.route('/api', methods=['POST'])
-def api():
-    bio = request.json['bio']
+def api_route():
+    bio = request.form.get('bio')
     stories = hn_parse(500)
     stories = relevance_reorder(stories, bio)
-    top_stories = [{"title": story.title, "url": story.url} for story in stories]
-    return jsonify(top_stories)
+    stories = [{"title": story.title, "url": f"https://news.ycombinator.com/item?id={story.item_id}"} for story in stories]
 
-if __name__ == "__main__":
-    flask.run(debug=True)
+    return jsonify(stories=stories)
+
+@flask.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
